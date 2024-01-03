c0de5849434d88dd115eac9e5bc8eafcd0a0fa76
LakeFox <mason@lakefox.net>
2024-01-03 01:29:21 -0600

    non working but flex element positioning is workin

--- cstyle/plugins/flex/main.go
+++ cstyle/plugins/flex/main.go
@@ -5,6 +5,8 @@ import (
 	"gui/cstyle"
 	"gui/element"
 	"gui/utils"
+	"slices"
+	"strings"
 )
 
 func Init() cstyle.Plugin {
@@ -12,6 +14,8 @@ func Init() cstyle.Plugin {
 		Styles: map[string]string{
 			"display":         "flex",
 			"justify-content": "*",
+			"flex-wrap":       "*",
+			"flex-direction":  "*",
 		},
 		Level: 1,
 		Handler: func(n *element.Node) {
@@ -19,31 +23,100 @@ func Init() cstyle.Plugin {
 			n.Width, _ = utils.ConvertToPixels("100%", n.EM, n.Parent.Width)
 
 			fmt.Println(n.Id)
-			siblings := n.Children
 
-			fmt.Println(len(siblings))
+			verbs := strings.Split(n.Styles["flex-direction"], "-")
 
-			tW := float32(0)
-			for _, v := range siblings {
-				tW += v.Width + v.Margin.Right + v.Margin.Left
+			orderedNode := order(*n, n.Children, verbs[0], len(verbs) > 1, n.Styles["flex-wrap"] == "wrap")
+
+			fmt.Println("SIASDJAOISJ")
+			for i, v := range orderedNode {
+				fmt.Println(i, len(v))
 			}
-			if tW > n.Width {
-				fmt.Println("TOO BIG", tW/float32(len(siblings)))
-				newSize := n.Width / float32(len(siblings))
-				tW = n.Width
-				for i := range siblings {
-					n.Children[i].Width = newSize
+
+			for _, row := range orderedNode {
+				for _, column := range row {
+					// add align-content justify-content and align-items logic here
+					// the elements are positioned correctly in the ordered array just need to calculate the px positions
 				}
-			}
-			fmt.Println(n.Width, tW)
-
-			fmt.Println(n.Styles["justify-content"])
-			y := siblings[0].Y
-			for i := range siblings {
-				fmt.Println(n.Children[i].Width)
-				n.Children[i].X += (((n.Width - tW) / float32(len(siblings))) * float32(i)) + (n.Children[i].Width * float32(i))
-				n.Children[i].Y = y
 			}
 		},
 	}
+}
+
+func order(p element.Node, elements []element.Node, direction string, reversed, wrap bool) [][]element.Node {
+	var dir, marginStart, marginEnd string
+	if direction == "column" {
+		dir = "Height"
+		marginStart = "Top"
+		marginEnd = "Bottom"
+	} else {
+		dir = "Width"
+		marginStart = "Left"
+		marginEnd = "Right"
+	}
+	max, _ := utils.GetStructField(&p, dir)
+
+	nodes := [][]element.Node{}
+
+	if wrap {
+		counter := 0
+		collector := []element.Node{}
+		for _, v := range elements {
+			elMax, _ := utils.GetStructField(&v, dir)
+			elMS, _ := utils.GetStructField(&v.Margin, marginStart)
+			elME, _ := utils.GetStructField(&v.Margin, marginEnd)
+			tMax := elMax.(float32) + elMS.(float32) + elME.(float32)
+			if counter+int(tMax) < int(max.(float32)) {
+				collector = append(collector, v)
+			} else {
+				if reversed {
+					slices.Reverse(collector)
+				}
+				nodes = append(nodes, collector)
+				collector = []element.Node{}
+				collector = append(collector, v)
+				counter = 0
+			}
+			counter += int(tMax)
+		}
+		if len(collector) > 0 {
+			nodes = append(nodes, collector)
+		}
+	} else {
+		var tMax float32
+		for _, v := range elements {
+			elMax, _ := utils.GetStructField(&v, dir)
+			elMS, _ := utils.GetStructField(&v.Margin, marginStart)
+			elME, _ := utils.GetStructField(&v.Margin, marginEnd)
+			tMax += elMax.(float32) + elMS.(float32) + elME.(float32)
+		}
+
+		pMax, _ := utils.GetStructField(&p, dir)
+
+		// Resize node to fit
+		if tMax > pMax.(float32) {
+			newSize := pMax.(float32) / float32(len(elements))
+
+			if dir == "Width" {
+				for _, v := range elements {
+					v.Width = newSize
+					nodes = append(nodes, []element.Node{v})
+				}
+				if reversed {
+					slices.Reverse(nodes)
+				}
+			} else {
+				nodes = append(nodes, []element.Node{})
+				for _, v := range elements {
+					v.Height = newSize
+					nodes[0] = append(nodes[0], v)
+				}
+				if reversed {
+					slices.Reverse(nodes[0])
+				}
+			}
+		}
+	}
+
+	return nodes
 }
